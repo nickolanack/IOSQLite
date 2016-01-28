a5d65a9d0b85428f2c0454800f4a3be20feeb81b
nickolanack <nickblackwell82@gmail.com>
2016-01-27 16:06:19 -0800

    remove Pods/ add to gitignore

--- Example/Pods/Target Support Files/IOSQlite/IOSQlite-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IOSQlite : NSObject
-@end
-@implementation PodsDummy_IOSQlite
-@end
--- Example/Pods/Target Support Files/Pods-IOSQlite_Tests/Pods-IOSQlite_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IOSQlite_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_IOSQlite_Tests
-@end
